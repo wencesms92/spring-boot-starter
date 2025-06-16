70a09ea411150f584e3ac9c0424731b2c90e779b
wencesms92 <wencesms92@gmail.com>
2025-06-16 21:32:27 +0200

    fix(value): add @value decorator for env property

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    @Value("${spring.dash-api-url}")
+    @Value("${dash-api-url}")
     private String dashApiUrl;
 
     @GetMapping("/")
